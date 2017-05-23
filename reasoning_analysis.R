0d9fc14eba4dd1d366b9d78bcb9b1fb87549fbf4
ajstewartlang <andrew.stewart@manchester.ac.uk>
2017-05-23 15:15:28 +0100

    First commit

---
+++ reasoning_analysis.R
@@ -0,0 +1,220 @@
+#load the appropriate packages
+
+library (lme4)
+library (lmerTest)
+library (lsmeans)
+library (readr)
+library (ggplot2)
+
+#in total there are 8 conditions
+#the second sentence is identical for the first 4 versions, and for the last 4 versions
+#the third sentence is identical across all conditions
+#for example:
+
+#1MP
+#If there is a black square then there is a yellow circle. 
+#There is a black square.
+#There is a yellow circle.
+
+#1AC
+#If there is a yellow circle then there is a black square. 
+#There is a black square.
+#There is a yellow circle.
+
+#1CB
+#There is a black square and there is a yellow circle. 
+#There is a black square.
+#There is a yellow circle.
+
+#1DB
+#There is a yellow circle or there is a black square.
+#There is a black square.
+#There is a yellow circle.
+
+#1MPF
+#If there is a black square then there is a yellow circle.
+#There is not a black square.
+#There is a yellow circle.
+
+#1ACF
+#If there is a yellow circle then there is a black square.
+#There is not a black square.
+#There is a yellow circle.
+
+#1CF
+#There is a yellow circle and there is a black square.
+#There is not a black square.
+#There is a yellow circle.
+
+#1DF
+#There is a black square or there is a yellow circle. 
+#There is not a black square.
+#There is a yellow circle.
+
+#FP = First Pass - the sum of all fixations within a region before the eye exits to the left or to the right
+#RP = Regressions Path - the sum of all fixations within a region (incl. re-reading of previous regions) before the eye exits to the right
+#TT = Total Time - the sum of all fixations within a region
+
+#Run the First Pass analysis
+#read in data file
+FP <- read_csv("~/Desktop/Air Work/R analyses/Henrik/FP.csv")
+
+#MP and AC analysis - all reading times (even when people get the conclusion wrong)
+#select MP and AC conditions
+index <- FP$CondType == "MP" | FP$CondType == "AC"
+MPACdataFP <- as.data.frame (FP[index,])
+
+#make CondType a factor
+MPACdataFP$CondType <- as.factor (MPACdataFP$CondType)
+
+#set up contrasts
+contrasts (MPACdataFP$CondType) <- matrix (c(.5, -.5)) 
+
+#Don't examine antecedent and consequent times as text differs - remember to look at % regression in later
+
+#Examine FP on the minor premise
+model.MPACFPpr <- lmer (Premise ~ CondType + (1+CondType|P.s) + (1+CondType|Item), data=MPACdataFP, REML=TRUE)
+summary (model.MPACFPpr)
+lsmeans (model.MPACFPpr, pairwise ~ CondType, adjust="none")
+
+#Examine FP on conclusion
+model.MPACFP <- lmer (Conclusion ~ CondType + (1+CondType|P.s) + (1+CondType|Item), data=MPACdataFP, REML=TRUE)
+summary (model.MPACFP)
+
+
+
+#Run the Regression Path analysis
+#read in data file
+RP <- read_csv("~/Desktop/Air Work/R analyses/Henrik/RP.csv")
+
+#select MP and AC conditions
+index <- RP$CondType == "MP" | RP$CondType == "AC"
+MPACdataRP <- as.data.frame (RP[index,])
+
+#make CondType a factor
+MPACdataRP$CondType <- as.factor (MPACdataRP$CondType)
+
+#set up contrasts
+contrasts (MPACdataRP$CondType) <- matrix (c(.5, -.5)) 
+
+#Don't examine antecedent and consequent times as text differs - remember to look at % regressions in later 
+
+#Examine RP on the minor premise
+model.MPACRPpr <- lmer (Premise ~ CondType + (1+CondType|P.s) + (1+CondType|Item), data=MPACdataRP, REML=TRUE)
+summary (model.MPACRPpr)
+lsmeans (model.MPACRPpr, pairwise ~ CondType, adjust="none")
+
+#Examine RP on conclusion
+model.MPACRP <- lmer (Conclusion ~ CondType + (1+CondType|P.s) + (1+CondType|Item), data=MPACdataRP, REML=TRUE)
+summary (model.MPACRP)
+lsmeans (model.MPACRP, pairwise ~ CondType, adjust="none")
+
+
+#Run the Total Time analysis
+#read in the data file
+TT <- read_csv("~/Desktop/Air Work/R analyses/Henrik/TT.csv")
+
+#select MP and AC conditions
+index <- TT$CondType == "MP" | TT$CondType == "AC"
+MPACdataTT <- as.data.frame (TT[index,])
+
+#make CondType a factor
+MPACdataTT$CondType <- as.factor (MPACdataTT$CondType)
+
+#set up contrasts
+contrasts (MPACdataTT$CondType) <- matrix (c(.5, -.5)) 
+
+#Don't examine antecedent and consequent times as text differs - remember to look at % regressions later
+
+#Examine TT on the minor premise
+model.MPACTTpr <- lmer (Premise ~ CondType + (1+CondType|P.s) + (1+CondType|Item), data=MPACdataTT, REML=TRUE)
+summary (model.MPACTTpr)
+lsmeans (model.MPACTTpr, pairwise ~ CondType, adjust="none")
+
+#Examine TT on conclusion
+model.MPACTT <- lmer (Conclusion ~ CondType + (1+CondType|P.s) + (1+CondType|Item), data=MPACdataTT, REML=TRUE)
+summary (model.MPACTT)
+lsmeans (model.MPACTT, pairwise ~ CondType, adjust="none")
+
+
+
+#This is now analysing a different two conditions
+#MP and MPF analysis (all reading times even when people get the conclusion wrong)
+#Run the First Pass analysis
+#read in the data file
+FP <- read_csv("~/Desktop/Air Work/R analyses/Henrik/FP.csv")
+
+#select MP and MPF conditions
+index <- FP$CondType == "MP" | FP$CondType == "MPF"
+MPACdataFP <- as.data.frame (FP[index,])
+
+#make CondType a factor
+MPACdataFP$CondType <- as.factor (MPACdataFP$CondType)
+
+#set up contrasts
+contrasts (MPACdataFP$CondType) <- matrix (c(.5, -.5)) 
+
+#Examine FP on the conditional consequent
+model.MPACFPpr <- lmer (Consequent ~ CondType + (1+CondType|P.s) + (1+CondType|Item), data=MPACdataFP, REML=TRUE)
+summary (model.MPACFPpr)
+lsmeans (model.MPACFPpr, pairwise ~ CondType, adjust="none")
+
+#Don't examine minor premise times as text differs - remember to look at % regression in later
+
+#Examine FP on conclusion
+model.MPACFP <- lmer (Conclusion ~ CondType + (1+CondType|P.s) + (1+CondType|Item), data=MPACdataFP, REML=TRUE)
+summary (model.MPACFP)
+
+
+#Run the Regression Path analysis
+#read in the data file
+RP <- read_csv("~/Desktop/Air Work/R analyses/Henrik/RP.csv")
+
+#select MP and MPF conditions
+index <- RP$CondType == "MP" | RP$CondType == "MPF"
+MPACdataRP <- as.data.frame (RP[index,])
+
+#make CondType a factor
+MPACdataRP$CondType <- as.factor (MPACdataRP$CondType)
+
+#set up contrasts
+contrasts (MPACdataRP$CondType) <- matrix (c(.5, -.5)) 
+
+#Examine RP on the conditional consequent
+model.MPACRPpr <- lmer (Consequent ~ CondType + (1+CondType|P.s) + (1+CondType|Item), data=MPACdataRP, REML=TRUE)
+summary (model.MPACRPpr)
+lsmeans (model.MPACRPpr, pairwise ~ CondType, adjust="none")
+
+#Don't examine minor premise times as text differs - remember to look at % regression in later
+
+#Examine RP on conclusion
+model.MPACRP <- lmer (Conclusion ~ CondType + (1+CondType|P.s) + (1+CondType|Item), data=MPACdataRP, REML=TRUE)
+summary (model.MPACRP)
+
+
+
+
+#Run the Total Time analysis
+#read in the data file
+TT <- read_csv("~/Desktop/Air Work/R analyses/Henrik/TT.csv")
+
+#select MP and MPF conditions
+index <- TT$CondType == "MP" | TT$CondType == "MPF"
+MPACdataTT <- as.data.frame (TT[index,])
+
+#make CondType a factor
+MPACdataTT$CondType <- as.factor (MPACdataTT$CondType)
+
+#set up contrasts
+contrasts (MPACdataTT$CondType) <- matrix (c(.5, -.5)) 
+
+#Examine TT on the conditional consequent
+model.MPACTTpr <- lmer (Consequent ~ CondType + (1+CondType|P.s) + (1+CondType|Item), data=MPACdataTT, REML=TRUE)
+summary (model.MPACTTpr)
+lsmeans (model.MPACTTpr, pairwise ~ CondType, adjust="none")
+
+#Don't examine minor premise times as text differs - remember to look at % regression in later
+
+#Examine TT on conclusion
+model.MPACTT <- lmer (Conclusion ~ CondType + (1+CondType|P.s) + (1+CondType|Item), data=MPACdataTT, REML=TRUE)
+summary (model.MPACTT)
